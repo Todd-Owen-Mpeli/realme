fe8f4f945971e82a21589fb4da03a13233a58b1e
Todd Mpeli <toddowenmpeli@rocketmail.com>
2022-08-30 23:48:41 +0100

    Edited the footer links mobile viewport structure and styling.

--- components/Footer.js
+++ components/Footer.js
@@ -9,13 +9,13 @@ const Footer = () => {
 			{/* <!--===== FOOTER =====--> */}
 			<footer className={styles.footer}>
 				<div className="container mx-auto">
-					<div className="content p-12 flex flex-col justify-between text-pureBlack">
-						<div className="contentTop flex justify-between">
-							<div class="recommended flex flex-col">
-								<h2 className="pb-6">
+					<div className="content p-8 pb-2 lg:p-12 flex flex-col justify-between text-pureBlack">
+						<div className="contentTop flex flex-col lg:flex-row justify-between">
+							<div className="recommended flex flex-col">
+								<h2 className="pt-0 pb-1 lg:pb-6">
 									<strong>Recommended</strong>
 								</h2>
-								<ul class="links flex flex-col">
+								<ul className="links flex flex-col">
 									{recommended.map((keys) => (
 										<li>
 											<FooterLink
@@ -27,11 +27,11 @@ const Footer = () => {
 									))}
 								</ul>
 							</div>
-							<div class="support flex flex-col">
-								<h2 className="pb-6">
+							<div className="support flex flex-col">
+								<h2 className="pt-8 pb-1 lg:pb-6">
 									<strong>Support</strong>
 								</h2>
-								<ul class="links flex flex-col">
+								<ul className="links flex flex-col">
 									{support.map((keys) => (
 										<li>
 											<FooterLink
@@ -43,11 +43,11 @@ const Footer = () => {
 									))}
 								</ul>
 							</div>
-							<div class="aboutRealme flex flex-col">
-								<h2 className="pb-6">
+							<div className="aboutRealme flex flex-col">
+								<h2 className="pt-8 pb-1 lg:pb-6">
 									<strong>About realme</strong>
 								</h2>
-								<ul class="links flex flex-col">
+								<ul className="links flex flex-col">
 									{aboutRealMe.map((keys) => (
 										<li>
 											<FooterLink
@@ -59,11 +59,11 @@ const Footer = () => {
 									))}
 								</ul>
 							</div>
-							<div class="contactRealme flex flex-col">
-								<h2 className="pb-6">
+							<div className="contactRealme flex flex-col">
+								<h2 className="pt-8 pb-1 lg:pb-6">
 									<strong>Contact realme</strong>
 								</h2>
-								<ul class="links flex flex-col">
+								<ul className="links flex flex-col">
 									<Link href="#" target="blank">
 										<a>service.eu@realme.com</a>
 									</Link>
@@ -72,10 +72,10 @@ const Footer = () => {
 									</Link>
 								</ul>
 							</div>
-							<div class="socialLinks flex flex-col items-center">
+							<div className="socialLinks mt-12 lg:mt-0 flex flex-col items-center">
 								<button className="mb-6 p-2 flex flex-col justify-between items-center border-[1px] border-solid border-black">
 									<a href="" target="blank">
-										<i class="bi bi-headset"></i>
+										<i className="bi bi-headset"></i>
 										<strong>CHAT SUPPORT</strong>
 									</a>
 								</button>
@@ -84,40 +84,40 @@ const Footer = () => {
 									10:00-19:00, Mon-Fri <br />
 									Including Holidays
 								</h2>
-								<div class="socialLinks flex flex-col">
+								<div className="socialLinks flex flex-col">
 									<a href="" target="blank">
-										<i class="bi bi-facebook"></i>
+										<i className="bi bi-facebook"></i>
 									</a>
 									<a href="" target="blank">
-										<i class="bi bi-twitter"></i>
+										<i className="bi bi-twitter"></i>
 									</a>
 									<a href="" target="blank">
-										<i class="bi bi-youtube"></i>
+										<i className="bi bi-youtube"></i>
 									</a>
 									<a href="" target="blank">
-										<i class="bi bi-instagram"></i>
+										<i className="bi bi-instagram"></i>
 									</a>
 								</div>
 							</div>
 						</div>
 						<div className={styles.contentBottom}>
-							<div className="flex flex-col mt-16">
-								<div class="top">
+							<div className="flex flex-col-reverse lg:flex-col my-8 lg:mt-16">
+								<div className="">
 									<h4>United Kingdom (English / GBP)</h4>
 								</div>
-								<div class={styles.borderlineMid}></div>
-								<div class="bottom flex gap-3 justify-between">
+								<div className={styles.borderlineMid}></div>
+								<div className="flex flex-col-reverse lg:flex-row gap-3 justify-between">
 									<h4>
-										<i class="far fa-copyright"></i>2022 realme. All Rights
+										<i className="far fa-copyright"></i>2022 realme. All Rights
 										Reserved.
 									</h4>
-									<div class="right flex gap-3 justify-end">
+									<div className="flex flex-col mb-8 lg:mb-0 lg:flex-row gap-1 lg:gap-3 lg:justify-center items-center lg:justify-end">
 										<a href="#cookiePolicy">Cookie Policy</a>
-										<div class="borderline"></div>
+										<div className="borderline"></div>
 										<a href="#termsOfSales">Terms of Sales</a>
-										<div class="borderline"></div>
+										<div className="borderline"></div>
 										<a href="#privacyPolicy">Privacy Policy</a>
-										<div class="borderline"></div>
+										<div className="borderline"></div>
 										<a href="#userAgreement">User Agreement</a>
 									</div>
 								</div>
