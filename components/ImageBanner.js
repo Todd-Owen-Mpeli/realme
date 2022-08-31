aeb090332d348417c4344066e7029c9a02773e1d
Todd Mpeli <toddowenmpeli@rocketmail.com>
2022-08-31 23:42:34 +0100

    Edited the import background image and poitioning etc via an object.

--- components/ImageBanner.js
+++ components/ImageBanner.js
@@ -1,30 +1,34 @@
 import Link from "next/link";
+import styles from "/styles/Home.module.scss";
 
 const ImageBanner = (props) => {
-	const backgroundImage = "/images/otherImages/2560-9addad5b18.jpg";
-	console.log(props.data.backgroundImage);
-
 	const backgroundImageProperties = {
-		backgroundImage: `url('${backgroundImage}')`,
+		backgroundImage: `url('${props.data.backgroundImage}')`,
+		backgroundPosition: "center",
+		backgroundRepeat: "no-repeat",
+		backgroundSize: "contain",
 	};
+
 	const buttonText = "Learn More";
 	const buttonLink = "https://www.realme.com/uk/brand";
 
 	return (
 		<>
 			<div
-				className="w-full h-[500px] flex flex-col justify-center items-center"
+				className="w-full h-[350px] flex flex-col justify-center items-center"
 				style={backgroundImageProperties}
 			>
-				<div className="flex flex-col text-white justify-center items-center">
-					<h2 className=" text-4xl">
-						<strong>Dare to leap</strong>
-					</h2>
-					<button className="py-2 px-10 mt-10 border-[1px] border-solid border-white hover:bg-white hover:text-black">
-						<Link href={buttonLink} target="blank">
-							<a>{buttonText}</a>
-						</Link>
-					</button>
+				<div className={styles.ImageBanner}>
+					<div className="flex flex-col text-white justify-center items-center">
+						<h2 className=" text-4xl">
+							<strong>Dare to leap</strong>
+						</h2>
+						<button className="py-2 px-10 mt-10 border-[1px] border-solid border-white hover:bg-white hover:text-black">
+							<Link href={buttonLink} target="blank">
+								<a>{buttonText}</a>
+							</Link>
+						</button>
+					</div>
 				</div>
 			</div>
 		</>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import Head from "next/head";
-
 import ImageBanner from "../components/imagebanner";
 
-const imagesProps = [
-	{
-		backgroundImage: "/images/otherImages/2560-9addad5b18.jpg",
-	},
-];
+const imagesProps = {
+	backgroundImage: "/images/otherImages/2560-9addad5b18.jpg",
+};
 
 export default function Home() {
 	return (
