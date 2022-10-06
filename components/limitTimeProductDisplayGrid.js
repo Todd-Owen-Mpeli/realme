e035335c806da1a3e6346a977e2b4744efcb991c
Todd Mpeli <toddowenmpeli@rocketmail.com>
2022-10-06 17:32:56 +0100

    Edited the limited time product card and gird display content structure and scss styling. Also edited the array of objects content.

--- components/limitTimeProductDisplayGrid.js
+++ components/limitTimeProductDisplayGrid.js
@@ -2,14 +2,14 @@ import LimitTimeProductCard from "./LimitTimeProductCard";
 import styles from "/styles/Home.module.scss";
 
 const LimitTimeProductDisplayGrid = (props) => {
-	const title = props.title;
-
 	return (
 		<>
 			<div className={styles.limitTimeProductDisplayGrid}>
 				<div className="container mx-auto p-4 lg:p-5">
 					<div className={styles.title}>
-						<h3 className="capitalize font-semibold mb-4 text-2xl">{title}</h3>
+						<h3 className="capitalize font-semibold mb-4 text-2xl">
+							{props.title}
+						</h3>
 					</div>
 					<div className="flex flex-col md:flex-row justify-between items-center gap-4">
 						{props.data.map((keys) => (
